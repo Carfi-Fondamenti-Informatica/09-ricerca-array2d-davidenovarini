bf27cf66b49eb1e510eaced204a89229f39447c6
davidenovarini <116503113+davidenovarini@users.noreply.github.com>
2022-12-15 21:45:44 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
+bool controllo(char nomi[10][20], char nome[20], int &c){
 
+    for (int n = 0; n < 10; n++)
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            if (nomi [n][i] != nome[i]){
+                i = 20;  //20 variabile che vanno da (0-19)
+            }
+            if (i == 19){ //se tutte le letteri sono uguali allora va bene
+                c = n;//posizione, essendo che è nell'array va da 0-19
+                return true;
+            }
+        }
+    }
+    return false;
+}
