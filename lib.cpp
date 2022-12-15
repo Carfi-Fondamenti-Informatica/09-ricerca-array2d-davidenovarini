d1e04d785d27fbb648c326c1d2859d6115424f11
davidenovarini <116503113+davidenovarini@users.noreply.github.com>
2022-12-15 21:49:51 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -4,7 +4,7 @@ bool controllo(char nomi[10][20], char nome[20], int &c){
     for (int i = 0; i < 20; i++)
         {
             if (nomi [n][i] != nome[i]){
-                i = 20;  //20 variabile che vanno da (0-19)}
+                i = 20;  }
             if (i == 19){
                 c = n;
                 return true;}
