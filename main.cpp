276b6eeee6f4b3291e58c8e5aa901051b5326e8d
davidenovarini <116503113+davidenovarini@users.noreply.github.com>
2022-12-15 21:46:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+
+    char lista[10][20];
+    char nome[20];
+    int c = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 20; j++)
+        {
+            nome[j] = ' ';
+            lista[i][j] = ' ';
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> lista[i];
+    }
+    cin >> nome;
+
+    if (controllo(lista, nome, c))
+    {
+        cout << c;
+    }
+    else
+    {
+        cout << "non presente"<<endl;
+    }
+
+    return 0;
 }
